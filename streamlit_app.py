225f8390b98e4b83dad3346b2e8adaa08ed1a378
Andrew <164203018+andrewnguyxn@users.noreply.github.com>
2024-07-24 08:15:48 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,13 @@
 import streamlit as st
+import pandas as pd
+
+# File uploader widget
+uploaded_file = st.file_uploader("Choose an EXCEL file", type="xlsx")
+
+if uploaded_file is not None:
+    # Read the CSV file
+    df = pd.read_xlsx(uploaded_file)
+
 
 st.title("🎈 My new app")
 st.write(
